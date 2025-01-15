bbc88b7e77c8c5ae64859f22db3357862b137838
Naveen K M <75108666+naveen059@users.noreply.github.com>
2025-01-15 14:32:38 +0530

    Update ViewProducts.jsx

--- frontend/src/components/ViewProducts.jsx
+++ frontend/src/components/ViewProducts.jsx
@@ -11,7 +11,7 @@ function ViewProducts() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get("https://productapplication.onrender.com/api/allProducts");
+        const response = await axios.get("https://product-backend-yxxe.onrender.com/api/allProducts");
         setProducts(response.data.data);
       } catch (error) {
         console.error("Error fetching products:", error);
